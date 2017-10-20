b3e30109c2d1f30fb090ce6b3879506197099a6f
Mike <pythomike@gmail.com>
2017-10-20 19:15:02 +0000

    Added completed form toggle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,3 @@
-//var db = require('../server/lib/in-memory-db.js')(db)
-// var data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "<script>alert('uh oh!')</script>"
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
- 
 $(function(){
 
   function renderTweets(tweets) {
@@ -59,11 +11,12 @@ $(function(){
       method: 'get',
       url: '/tweets/',
     }).done(function(json) {
-    renderTweets(json)
+    renderTweets(json)  
     })
   }
 
 function createTweetElement(tweet) {
+let timeStamp = moment(tweet.created_at).fromNow()
   return $('<article>', {
     'class': 'tweet',
     html: [
@@ -91,7 +44,7 @@ function createTweetElement(tweet) {
             'class': 'footer-time',
             html: [
               $('<p>', {
-                text: tweet.created_at
+                text: timeStamp
               })
             ]
           }),
@@ -114,6 +67,11 @@ function createTweetElement(tweet) {
     ]
   });
 }
+
+$("#compose").on("click", function() { 
+  $(".container").find(".new-tweet").slideToggle()
+  $(".container").find('textarea').focus();
+})
 
   $('#new-tweet').on('submit', function (event) {
     event.preventDefault();
