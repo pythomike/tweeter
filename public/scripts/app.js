ec0f87ce5aab8755153f342b5e67e01c816df554
Mike <pythomike@gmail.com>
2017-10-20 02:12:12 +0000

    added form validation and refetch tweets on submission - nothing is currently broken!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,50 +1,50 @@
 //var db = require('../server/lib/in-memory-db.js')(db)
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "<script>alert('uh oh!')</script>"
-    },
-    "created_at": 1461113796368
-  }
-];
+// var data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": {
+//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//       },
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": {
+//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+//       },
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   },
+//   {
+//     "user": {
+//       "name": "Johann von Goethe",
+//       "avatars": {
+//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+//       },
+//       "handle": "@johann49"
+//     },
+//     "content": {
+//       "text": "<script>alert('uh oh!')</script>"
+//     },
+//     "created_at": 1461113796368
+//   }
+// ];
  
 $(function(){
 
@@ -54,7 +54,7 @@ $(function(){
     }
   }
   
-  function getTweets(){
+  function loadTweets(){
     $.ajax({
       method: 'get',
       url: '/tweets/',
@@ -118,24 +118,25 @@ function createTweetElement(tweet) {
   $('#new-tweet').on('submit', function (event) {
     event.preventDefault();
     var textArea = this;
-    console.log(this)
-    var mdata = $(this).serialize();
-    console.log(mdata)
-    $.ajax({
-      method: 'post',
-      url: '/tweets/',
-      data: mdata
-    }).done(function () {
-      textArea.reset();
-
-      renderTweets(data);
-    });
+    var data = $(this).serialize();
+    var charCheck = data.length -5
+    
+    if (charCheck > 140){
+      alert("Please make your tweet 140 characters or fewer")
+    } else if (charCheck < 1){
+      alert("You need to actually tweet something though...")
+    } else {
+      $.ajax({
+        method: 'post',
+        url: '/tweets/',
+        data: data
+      }).done(function () {
+        textArea.reset();
+      })
+        loadTweets();             //Render tweets no longer works...da fuq?!
+    }
   })
-
-  // Why does renderTweets error out when it is in the event handler for the form submission?
-  // There isn't enough info in here, where am I calling the data-helpers functions
-  
-  
-  getTweets()
+    
+  loadTweets()
 
 })
